ddcdf90f44127b981c0bab4cee57c50ea3b1bdff
beat-kuliah <if-18056@students.ithb.ac.id>
2022-10-18 18:52:33 +0700

    skeleton

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// )
